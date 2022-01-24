a98494fa2b37e661b39f5b651a3a52dfd17ca822
marcela <marceg8810@gmail.com>
2022-01-24 12:27:45 -0500

    tareas

--- src/App.js
+++ src/App.js
@@ -1,34 +1,71 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 
 /// Modifica el componente para que se puedan agregar tareas, tachar y destacharlas y error de validacion en el input
+export default function App() {
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      tasks: [
-        { id: 1, name: "Sacar la ropa", done: false },
-        { id: 2, name: "Hacer la cama", done: true },
-        { id: 3, name: "Leer un rato", done: false }
-      ],
-      newTask: ''
-    }
+  const [tasks, setTasks] = useState(
+    [
+      { id: 1, name: "Sacar la ropa", done: false },
+      { id: 2, name: "Hacer la cama", done: true },
+      { id: 3, name: "Leer un rato", done: false }
+    ],
+  );
+
+  const [newTask, setNewTask] = useState('');
+  const [error, setError] = useState(false);
+
+  const addTask = (e) => {
+    setError(false);
+    setNewTask(e.target.value);
+    e.target.value = " ";
   }
-  render() {
-    return (
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    if(newTask !== ''){
+      const optAdd = [...tasks];
+      const addNew = {
+        id: tasks.length + 1,
+        name: newTask,
+        done: false,
+      }
+      optAdd.push(addNew);
+      setTasks(optAdd);
+      setError(false)
+      setNewTask('');
+    } else {
+      setError(true);
+    }    
+  }
+
+  const deleteTask = (e) => {
+    e.stopPropagation();
+    setTasks(tasks.filter(task => task.id !== parseInt(e.target.id, 0)))
+  }
+
+  const doneTask = (i) => {
+    const auxTask = [...tasks];
+    auxTask[i].completed = !auxTask[i].completed;
+    setTasks(auxTask);
+  }
+
+  return(
+    <div>
       <div className="wrapper">
         <div className="list">
-          <h3>Por hacer:</h3>
-          <ul className="todo">
-            {this.state.tasks.map((task, index) => <li key={task.id}>{task.name}</li>)}
+          <h3>Tareas:</h3>
+          <ul className="todo" >
+           {tasks.map((task, index) => <li onClick={() => doneTask(index)} key={task.id} className={ task.completed ? "done" : null }>
+             {task.name}
+             <a onClick={deleteTask} id={task.id} >      x</a>
+           </li>
+           )}
           </ul>
-          <form>
-            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={this.state.newTask} />
+          <form onSubmit={submitHandler}>
+            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" value={newTask} onChange={addTask} className={`${error ? "error" : ""}` }/>
           </form>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
-
-export default App;
